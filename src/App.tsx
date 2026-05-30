c6c50812b6c3421df1d60e81d7795b26b22868f7
ashlesharm <ashlesharm@youremail.com>
2026-05-30 20:59:10 +0530

    fix: FlashTicker maybeSingle + realtime subscription

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import Login from './pages/Login';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import Terms from './pages/Terms';
 import { authService } from './lib/auth';
+import FlashTicker from './components/FlashTicker';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -28,6 +29,7 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
+      <FlashTicker />
       <main>{children}</main>
       <Footer />
     </>
@@ -61,12 +63,11 @@ function AppRoutes() {
 
         {/* Login/Admin */}
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<ProtectedAdminRoute />} />
+<Route path="/admin" element={<ProtectedAdminRoute />} />
       </Routes>
     </>
   );
 }
-
 export default function App() {
   return (
     <BrowserRouter>
